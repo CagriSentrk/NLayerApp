691856624335ce685ea181aa1bd5c56c607bdd79
Symbytex <cagrisntrk0614@gmail.com>
2022-12-21 01:09:43 +0300

    .

--- NLayer.Core/BaseEntity.cs
+++ NLayer.Core/BaseEntity.cs
@@ -11,6 +11,8 @@ namespace NLayer.Core
         //Abstract kullanılarak tutulur.
     {
         public int Id { get; set; }
+
+                    
         public DateTime CreateDate { get; set; }
         public DateTime UpdateDate { get; set; }
 
